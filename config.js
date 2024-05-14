8014579a88902dce9c5951ba43be642ce0c7d80d
SACHIN GARG <39459615+sachingarg0@users.noreply.github.com>
2024-05-14 15:30:56 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+var MID = "PREROG28318988834649";
+var MKEY = "KGUOnMoZuqoVtTR&";
+
+var ENV = "securegw.paytm.in";
+var WEBSITE = "DEFAULT";
+
+exports.MID = MID;
+exports.MKEY = MKEY;
+exports.ENV = ENV;
+exports.WEBSITE = WEBSITE;
